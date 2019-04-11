fdb8be136df173c4ec1c166849aa3300af47004d
sparklfox <actuallyori@gmail.com>
2019-04-11 11:12:54 +0100

    error handler compile

--- dist/client/Client.js
+++ dist/client/Client.js
@@ -84,7 +84,7 @@ class SparklClient extends events_1.EventEmitter {
             this.logger.warn("It is not recommended to proceed with your current token, as your account may be terminated.");
             this.logger.warn("You can read more here: https://discordapp.com/guidelines");
         }
-        this.on("error", this.logger.error);
+        this.discord.on("error", (e) => this.logger.error(e));
         this.emit("ready");
         return this;
     }
